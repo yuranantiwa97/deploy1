93d669b79e30134cffc1d652e636a1a8301cc1a1
yuranan tiwa <yuranantiwa97@students.amikom.ac.id>
2025-08-11 10:22:52 +0700

    chore: expose Flask WSGI app as pp for Vercel

--- api/index.py
+++ api/index.py
@@ -1,9 +1,4 @@
 # api/index.py
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # agar app.py bisa diimport
-
-from vercel_wsgi import handle_request
-from app import app  # Flask instance di app.py
-
-def handler(request, context):
-    return handle_request(app, request, context)
+from app import app  # <- WSGI application (Flask)
